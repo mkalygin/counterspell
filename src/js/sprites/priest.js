6a24da8b1f26d6a7b4da53eace71e5958920b9c8
Giorgiy Lutidze <giorgii.lutidze@gmail.com>
2022-01-08 19:06:49 +0300

    test commit

--- src/js/sprites/priest.js
+++ src/js/sprites/priest.js
@@ -7,6 +7,7 @@ class PriestSprite extends Phaser.GameObjects.Sprite {
     this.scene.add.existing(this);
     this.scene.physics.world.enableBody(this);
     this.scene.physics.add.collider(this, collider);
+    //test
 
     this.setTexture('player', 'priest.0');
 
